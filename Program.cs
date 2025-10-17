5915a0f5ca4e41e84a65abb7c50bfc2ed3cd8830
Hellen <cristinyhellen75@gmail.com>
2025-10-17 20:44:39 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+string senhaDigitada;
+string senhaCorreta = "Senha123";
+
+Console.Write("Digite a senha: ");
+senhaDigitada = Console.ReadLine()!;
+bool acessoPermitido = senhaDigitada == senhaCorreta;
+if (acessoPermitido)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Acesso Permitido");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Acesso Negado");
+}
+
+Console.ResetColor();
